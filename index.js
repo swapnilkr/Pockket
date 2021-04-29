9194bd63e2e9ed942ae49e3e17ebbeb354c241af
Swapnil <swapnilkr099@gmail.com>
2021-04-29 10:58:28 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port,function(err){
+    if (err){
+        // interpolation
+        console.log(`Error in running the server : ${err}`);
+    }
+
+})
